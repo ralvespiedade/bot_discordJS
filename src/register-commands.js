11ed30b22e2978d8baec4b36d8852f49484ff2ba
Ricardo Alves <ralvespiedade@gmail.com>
2023-12-18 07:03:03 -0300

    integrando comando de slash

---
+++ src/register-commands.js
@@ -0,0 +1,28 @@
+require('dotenv').config();
+const { REST, Routes } = require('discord.js');
+
+const commands = [
+  {
+    name: 'hey',
+    description: 'reply with hey'
+  }
+];
+
+const rest = new REST({ version: '10' }).setToken(process.env.TOKEN);
+
+//Função que registra os comandos
+(async () => {
+  try {
+    console.log('Registrando comandos de slash "/"...');
+
+    
+    await rest.put(
+      Routes.applicationGuildCommands(process.env.CLIENT_ID, process.env.GUILD_ID),
+      { body: commands }
+    )
+
+    console.log('Comandos de slash "/" registrados com sucesso!')
+  } catch (error) {
+    console.error(`Ocorreu um erro: ${error}`);
+  }
+})();
